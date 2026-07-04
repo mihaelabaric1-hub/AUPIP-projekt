a581b40aeccef4d07c682a5be3f8de8d1f92e4ff
mihaelabaric1-hub <mihaela.baric1@fpmoz.sum.ba>
2026-07-04 15:53:09 +0200

    frontend initial commit

---
+++ aup-frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="text-center mt-10">
+    <v-card class="pa-6 mx-auto" max-width="500">
+      <h1>🎵 Music App</h1>
+      <p class="mt-2">CRUD aplikacija za artiste</p>
+
+      <v-divider class="my-4" />
+
+      <v-btn color="primary" to="/artists">
+        Otvori Artists
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+</script>
